e4559b536cd545b90eb14b2cc8701f71c48c6afb
mashhisyam <haryoh48@gmail.com>
2021-10-06 12:57:44 +0700

    Membuat Tampilan Untuk Inputan Nama

---
+++ app/src/main/java/org/aplas/miniproject1/ListJadwal.java
@@ -0,0 +1,14 @@
+package org.aplas.miniproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListJadwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listjadwal);
+    }
+}
