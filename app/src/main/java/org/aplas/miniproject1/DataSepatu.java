35c7cb12cfc1198bbcf5ee0f28dc113c23b2ac61
mashhisyam <haryoh48@gmail.com>
2021-10-09 21:14:34 +0700

    Memasukkan Resource Gambar

---
+++ app/src/main/java/org/aplas/miniproject1/DataSepatu.java
@@ -0,0 +1,32 @@
+package org.aplas.miniproject1;
+
+public class DataSepatu {
+    private static String [] namaSepatu ={
+            "Nike Kyrie 7",
+            "Nike Cosmic Unity",
+            "Nike Kobe V Protro Bruce Lee",
+            "Nike Kyrie 7 EP",
+            "Nike PG 5",
+            "Nike KD 14 EP",
+            "Nike Lebron 18",
+            "Nike Lebron 18 Low",
+            "Nike Kyrie Flytrap",
+            "Nike PG 4",
+            "Adidas D.O.N Issue 2",
+            "Adidas Harden Vol 4",
+    };
+    private static String [] detailSepatu ={
+            "This Kyrie 7 “raygun” Features A Textile Upper In A Mix Of Black And Orange With Yellow Stitched Swooshes On The Sides. Other Details Includes A The “raygun” Mascot Above The Toe And Yellow Heel. A Black Midsole Atop A White Rubber Outsole Completes The D",
+            "It's Made From At Least 25% Recycled Material By Weight And Comes Equipped With A Partially Recycled Air Zoom Strobel Designed To Help You Be Efficient Off The Dribble Or Effective On Defence.",
+            "This Nike Kobe 5 Protro Features A Red And Black Equipped With The Classic Snakeskin Texture Throughout The Upper. White Swooshes On The Sides Atop A Matching Colored Midsole And Translucent Outsole Completes The Design.",
+            "Quick First Step The Large Forefoot Air Zoom Turbo Unit Is Curved To Bend In Multiple Directions. It Provides Energy Return For Responsive Cornering",
+            "The Pg 5 Ep Combines The Agile Low-top Profile Paul Likes With Flexible Nike Air Dot Weld Strobel Cushioning. The Full-length Air Unit Is Stitched Directly To The Upper, Bringing The Plush, Bouncy Cushioning Right Up Under The Foot—perfect For Keeping Pg",
+            "The Full-length Air Zoom Strobel Unit Is Stitched Directly To The Upper. It Helps Cut Unnecessary Weight And Provides Full-foot Responsiveness. Cushlon Foam Adds A Soft, Comfortable Sensation.",
+            "from The City Of Akron, Ohio Moulded Into The Left Midsole, established Nineteen Eighty Four In The Right Midsole. Moulded, Padded Collar Fits Snugly Around The Ankle",
+            "This Low-top Nike Lebron 18 Ditches The Knitposite 2.0 Upper For A Mesh-like Exterior With Molded/padded Ankle Support. Thick Taping Extends From The Midfoot To The Forefoot With An Air Max Heel Unit Atop A Translucent Outsole Completes The Design.",
+            "Built For Kyrie Irving's Unmatchably Quick Game, The Kyrie Flytrap 3 Combines Full-foot Support And Containment, Responsive Cushioning And An Aggressive Outsole That's Contoured For Seamless Movement Through Impossibly Tight Lanes",
+            "Paul George Is The Rare High-percentage Shooter Who's Also A Coach's Dream On D. Designed For His Unrivaled 2-way Game, The Pg 4 Unveils A New Cushioning System That's Lightweight, Articulated And Responsive, Ideal For Players Like Pg Who Go Hard Every Pl",
+            "Inspired By The Iconic And Creative Courts Scattered Throughout New York City, These Basketball Shoes Have A Purple Upper With Hints Of Orange And Blue That Represent The Unique And Colourful Expressions Of The City That Raised Spida.",
+            "Armed With The Style Of The Mvp Player You Admire, Go To The Court. James Harden's Signature Shoes Proposed By This Adidas Basketball Feature A Significantly Lighter Midsole. A Pair That Supports The Feet Of Players Running Around The Court At Full Speed."
+    };
+}
