71636ddfe102871e074917586aa8d51c7d6bd1da
mashhisyam <haryoh48@gmail.com>
2021-10-09 21:16:18 +0700

    Membuat Activity

--- app/src/main/java/org/aplas/miniproject1/MainActivity.java
+++ app/src/main/java/org/aplas/miniproject1/MainActivity.java
@@ -2,13 +2,24 @@ package org.aplas.miniproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button kirim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kirim = findViewById(R.id.kirim);
+        kirim.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), ListSepatu.class));
+            }
+        });
+
     }
 }
