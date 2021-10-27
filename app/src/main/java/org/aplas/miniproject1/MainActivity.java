bb66e8538727b5bc04130d8c1fdb75579f85b417
mashhisyam <haryoh48@gmail.com>
2021-10-27 10:34:35 +0700

    Menambahkan README

--- app/src/main/java/org/aplas/miniproject1/MainActivity.java
+++ app/src/main/java/org/aplas/miniproject1/MainActivity.java
@@ -7,19 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     Button kirim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        kirim = findViewById(R.id.kirim);
-        kirim.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                startActivity(new Intent(getApplicationContext(), ListSepatu.class));
+        timer =new Timer();
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run(){
+                Intent intent=new Intent (MainActivity.this, ListSepatu.class);
+                startActivity(intent);
+                finish();
             }
-        });
+        },2000);
+    };
 
-    }
 }
